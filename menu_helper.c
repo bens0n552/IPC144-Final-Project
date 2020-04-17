4b259701ad929fb8ab3a162d37b1f121655d8bc9
BensonZhaoLangLi <63826923+BensonZhaoLangLi@users.noreply.github.com>
2020-04-17 18:47:43 -0400

    Add files via upload

--- menu_helper.c
+++ menu_helper.c
@@ -50,7 +50,7 @@ char getCategory(const struct RiderInfo info[], int realSize)
 	char category = 'x';
 	char newline = 'x';
 
-	/*---------------Promt the user to enter correct input---------------*/
+	//Input Validation: user must enter valid character followed by newline
 
 	printf("\nWhich category (S, M, L): ");
 	scanf("%c%c", &category, &newline);
@@ -70,7 +70,7 @@ char getCategory(const struct RiderInfo info[], int realSize)
 		}
 	}
 
-	/*---------------Convert input to upper case---------------*/
+	//Convert category inputs to highercase before returning value to caller function
 	if (category == 's') category = 'S';
 	else if (category == 'm') category = 'M';
 	else if (category == 'l') category = 'L';
@@ -111,10 +111,10 @@ void displayTime(double start, double finish, int withdrawn)
 		printf("  N/A");
 		return;
 	}
-
+	//Casting to convert decimal hours to integer hour plus integer minutes
 	hour = (int)timeDifference;
 	minute = (int)(timeDifference * 60) % 60;
-
+	//Logic to determine whether the character '0' should be included before minutes to ensure HH:MM format 
 	if (minute < 10)
 	{
 		printf(" %d:0%d", hour, minute);
@@ -130,7 +130,7 @@ void displayTime(double start, double finish, int withdrawn)
 void displayWithdrawn(int withdrawnStatus)
 {
 	char status[4];
-
+	//Assign and print Yes or No status depending on whether the rider withdrew from race 
 	if (withdrawnStatus == 0)
 	{
 		strcpy(status, "No");
@@ -163,7 +163,7 @@ void managerSystem(void)
 	FILE* data = fopen("data.txt", "r");
 	struct RiderInfo info[SIZE] = { 0 };
 
-	int realSize;
+	int realSize;    //number of lines in the file containing a record
 	int result;
 	int i;
 	int stop = 0;
@@ -177,10 +177,10 @@ void managerSystem(void)
 		for (i = 0; i < SIZE && stop == 0; i++)
 		{
 			result = readFileRecord(data, &info[i]);
-			if (result == 1)
+			if (result == 1)        //stop once EOF is reached, and result set to 1         
 			{
 				realSize = i;
-				stop = 1;
+				stop = 1;       
 			}
 		}
 	}
@@ -221,19 +221,19 @@ void managerSystem(void)
 
 	return;
 }
-
+//sorts all entries from fastest time to slowest time
 void sortInfo(struct RiderInfo info[], int realSize)
 {
 	int i, j;
-	struct RiderInfo temp = { 0 };
-	double timeDif1;
-	double timeDif2;
-
+	struct RiderInfo temp = { 0 }; //
+	double timeDif1;    //difference between start and finishing time for current element (j)
+	double timeDif2;	//difference between start and finishing time for next element (j + 1)
+	//bubble-sort algorithim
 	for (i = 0; i < realSize - 1; i++)
 	{
 		for (j = 0; j < realSize - i - 1; j++)
 		{
-			if (info[j].withdrawn == 1)
+			if (info[j].withdrawn == 1) //If rider is withdrawn, set timeDIf1 to implausibly high-value, to gaurauntee swapping with next element (moving down the array)
 			{
 				timeDif1 = 1000;
 			}
@@ -249,7 +249,7 @@ void sortInfo(struct RiderInfo info[], int realSize)
 			{
 				timeDif2 = info[j + 1].finishTime - info[j + 1].startTime;
 			}
-			if (timeDif1 > timeDif2)
+			if (timeDif1 > timeDif2)   
 			{
 				temp = info[j];
 				info[j] = info[j + 1];
@@ -264,7 +264,7 @@ void sortInfo(struct RiderInfo info[], int realSize)
 void displayTopThreeRiders(struct RiderInfo info[], int realSize)
 {
 	int i;
-	int matches = 0;
+	int matches = 0; //count number of matches for entries belonging to chosen category and no withdrawn rider
 
 	char category;
 
@@ -274,7 +274,7 @@ void displayTopThreeRiders(struct RiderInfo info[], int realSize)
 
 	printf("\nRider                    Age Group Time\n");
 	printf("---------------------------------------\n");
-	for (i = 0; i < realSize && matches < 3; i++)
+	for (i = 0; i < realSize && matches < 3; i++)  //For loop ends when it searches over all entries or 3 sucessfully matched entries 
 	{
 		if (info[i].raceLength == category && info[i].withdrawn != 1)
 		{
@@ -299,7 +299,7 @@ void displayInfo(struct RiderInfo info[], int realSize)
 
 	sortInfo(info, realSize);
 
-	printf("\nRider                    Age Group Time Withdrew\n"); //-25s 9s
+	printf("\nRider                    Age Group Time Withdrew\n"); 
 	printf("------------------------------------------------\n");
 	for (i = 0; i < realSize; i++)
 	{
@@ -321,30 +321,32 @@ void displayLastThreeRiders(struct RiderInfo info[], int realSize)
 	int i;
 	int j = 0;
 	int matches = 0;
-	int matchedElements[3] = { 0 }; 
+	int matchedElements[3] = { 0 };  //stores the index for elements that match the category and withdrawn status
 
 	char category;
 
 	category = getCategory(info, realSize);
 
-	sortInfo(info, realSize);
+	sortInfo(info, realSize);   //sorts info in descending order (slowest times are at the back)
 
 	printf("\nRider                    Age Group Time\n");
 	printf("---------------------------------------\n");
-
+	//Search entries from back to front in order to match 3 slowest times for a category
+	//if we display the three matches from this for loop, it will give us the correct entries but ordered backwards (slowest to fastest).
+	//This is why we must store the indexes for the matched arrays, but save them to be displayed in reverse later
 	for (i = realSize; i > 0 && matches < 3; i--)
 	{
 		if (info[i].raceLength == category && info[i].withdrawn != 1)
 		{
-			matchedElements[j] = i;
-			j++;
+			matchedElements[j] = i;          //assigned matched element's index to array
+			j++;							//update j, next matched element's index will be stored in empty "matchedElements[3]" element
 			matches++;
 		}
 	}
-
-	for (i = 2; i >= 0; i--)
+	//iterate backwards through matchedElements, to display matched elements in the reverse order they were found
+	for (i = 2; i >= 0; i--)      
 	{
-		j = matchedElements[i];
+		j = matchedElements[i]; //take the index stored in matched elements and use them to display the respective entries in info[]
 		printf("%-25s", info[j].name);
 		displayAgeGroup(info[j].age);
 		displayTime(info[j].startTime, info[j].finishTime, info[j].withdrawn);
@@ -363,7 +365,7 @@ void displayWinners(struct RiderInfo info[], int realSize)
 
 	printf("\nRider                    Age Group Category Time\n"); 
 	printf("------------------------------------------------\n");
-	for (i = 0, matches = 0; i < realSize && matches < 1; i++)
+	for (i = 0, matches = 0; i < realSize && matches < 1; i++)   //we only need the first matched entry, from a sorted records
 	{
 		if (info[i].raceLength == 'S' && info[i].withdrawn != 1)
 		{
@@ -375,7 +377,7 @@ void displayWinners(struct RiderInfo info[], int realSize)
 			matches++;
 		}
 	}
-
+	//If not a single matching entry is found after iterating through info[], it means no entries for a category is found
 	if (matches == 0)
 	{
 		printf("Not Awarded\n");
